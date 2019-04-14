4f841d8fee61df2bb9ef804c058d2661b7994a12
Franklin <franklin.tarke@gmail.com>
2019-04-14 15:14:06 -0700

    hello

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-import pycom
+from network import LoRa
+import socket
+import machine
 import time
 
-pycom.heartbeat(True)
+# initialise LoRa in LORA mode
+# Please pick the region that matches where you are using the device:
+# Asia = LoRa.AS923
+# Australia = LoRa.AU915
+# Europe = LoRa.EU868
+UnitedStates = LoRa.US915
+# more params can also be given, like frequency, tx power and spreading factor
+lora = LoRa(mode=LoRa.LORA, region= LoRa.US915)
 
+# create a raw LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
 while True:
-    pycom.rgbled(0xFF0000)  # Red
-    time.sleep(1)
-    pycom.rgbled(0x00FF00)  # Green
-    time.sleep(1)
-    pycom.rgbled(0x0000FF)  # Blue
-    time.sleep(1)
+    # send some data
+    s.setblocking(True)
+    s.send('Hello')
+
+    # get any data received...
+    s.setblocking(False)
+    data = s.recv(64)
+    print(data)
+
+    # wait a random amount of time
+    time.sleep(machine.rng() & 0x0F)
