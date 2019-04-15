3c427cb93f5c5d5ee06bd6110485a34fec48658e
eluengo7 <49622753+eluengo7@users.noreply.github.com>
2019-04-15 12:14:35 -0700

    Stage all

--- LoRaTalking1/main.py
+++ LoRaTalking1/main.py
@@ -1,1 +1,17 @@
-from PyMesh import Loramesh
+from network import LoRa
+import socket
+import time
+
+# Please pick the region that matches where you are using the device:
+# Asia = LoRa.AS923
+# Australia = LoRa.AU915
+# Europe = LoRa.EU868
+# United States = LoRa.US915
+lora = LoRa(mode=LoRa.LORA, region=LoRa.US915)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+
+while True:
+    if s.recv(64) == b'Ping':
+        s.send('Pong')
+    time.sleep(5)
